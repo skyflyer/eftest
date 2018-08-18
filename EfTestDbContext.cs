a029f7201559f28a2f76be3b47c4fed123391725
mihavalencic <miha.valencic@gmail.com>
2018-08-18 23:18:58 +0200

    Fixing logging with InMemory

--- EfTestDbContext.cs
+++ EfTestDbContext.cs
@@ -5,17 +5,21 @@ namespace eftest
 {
     public class EfTestDbContext : DbContext
     {
+        private static LoggerFactory loggerFactory;
+
+        static EfTestDbContext() {
+            loggerFactory = new LoggerFactory();
+            loggerFactory.AddProvider(new EFLoggingProvider());
+        }
+
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseInMemoryDatabase("test");
+            optionsBuilder.UseInMemoryDatabase("testing");
             // optionsBuilder.UseSqlite("Filename=db.sqlite3");
-
-            var lf = new LoggerFactory();
-            lf.AddProvider(new EFLoggingProvider());
-            // optionsBuilder.UseLoggerFactory(lf);
+            optionsBuilder.UseLoggerFactory(loggerFactory);
         }
     }
 }
